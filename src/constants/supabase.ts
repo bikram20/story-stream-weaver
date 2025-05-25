7e80370e7b1fa260f998bbe285c8b7aa294be1ee
Bikram Gupta <50128297+bikram20@users.noreply.github.com>
2025-05-25 01:12:10 -0700

    fix deploy issue by adding start directive to package.json

--- src/constants/supabase.ts
+++ src/constants/supabase.ts
@@ -1,3 +1,2 @@
-
-export const SUPABASE_URL = "https://kczezulhpklofnibgarp.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjemV6dWxocGtsb2ZuaWJnYXJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxNTM4MDcsImV4cCI6MjA2MzcyOTgwN30.uVVDMNPzlbxPAYcv9aO7HwItybKkifxlz-XRYLhF_8o";
+export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "https://kczezulhpklofnibgarp.supabase.co";
+export const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjemV6dWxocGtsb2ZuaWJnYXJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxNTM4MDcsImV4cCI6MjA2MzcyOTgwN30.uVVDMNPzlbxPAYcv9aO7HwItybKkifxlz-XRYLhF_8o";
